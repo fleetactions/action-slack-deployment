e60cd7df463f68ce513995f1c18bb1d58bb21999
Pav <n1teleet@gmail.com>
2022-03-18 23:39:02 +1100

    added support for alternate deployment message based on job.status

--- utils/format_message.js
+++ utils/format_message.js
@@ -7,15 +7,28 @@ const commitHash = process.env.GITHUB_SHA;
 const userName = process.env.GITHUB_ACTOR;
 const environmentName = process.env.ACTION_ENVIRONMENT;
 const deploymentType = (process.env.ACTION_DEPLOYMENT_TYPE) ? process.env.ACTION_DEPLOYMENT_TYPE : '';
+const jobStatus = process.env.ACTION_JOB_STATUS;
 
 const branchUrl = `https://github.com/${ownerWithRepositoryName}/tree/${branchName}`;
 const commitUrl = `https://github.com/${ownerWithRepositoryName}/commit/${commitHash}`;
 const userUrl = `https://github.com/${userName}`;
 
 const firstRow = `*${environmentName}* | *${repositoryName}* | *${deploymentType}*`.toUpperCase().replace(/ \| \*\*/g, '');
-const secondRow = `Branch <${branchUrl}|${branchName}> (<${commitUrl}|${commitHash.substring(0,7)}>)`;
-const thirdRow = `Deployed by <${userUrl}|@${userName}>`;
+const branch = `Branch <${branchUrl}|${branchName}> (<${commitUrl}|${commitHash.substring(0,7)}>)`;
+let deploymentMessage = '';
 
-const message = `${firstRow}\n${secondRow}\n${thirdRow}`;
+switch (jobStatus) {
+  case 'success':
+    deploymentMessage = `Deployed by <${userUrl}|@${userName}>`;
+    break;
+  case 'failure':
+    deploymentMessage = `Deployment by <${userUrl}|@${userName}> has *failed*`;
+    break;
+  case 'cancelled':
+    deploymentMessage = `Deployment by <${userUrl}|@${userName}> was *cancelled*`;
+    break;
+}
+
+const message = `${firstRow}\n${branch} | ${deploymentMessage}`;
 
 console.log(message);
